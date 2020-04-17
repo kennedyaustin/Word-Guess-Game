c6ce0481efd5eb367f0719de4e21442fe7a74430
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-04-17 19:18:11 -0400

    got most of the JS done, debugging time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,149 @@
-$(document).ready(function() {
-
-    let artists = ["Yunomi", "Stessie", "Honeycomebear", "Madeon", "Pikasonic"];
-    let guessedLetters = [];
-    let guessingArtist = [];
-    let artistName
-    let numberofGuesses
-    let wins = 0;
-    let winSound1= new Audio("../music/HoneyComeBear - Dear.mp3")
-
-    function startGame() {
-
-        artistName = artists[Math.floor( Math.random() * artists.length)];
-
+let artists = ["Yunomi", "Stessie", "Honeycomebear", "Madeon", "Pikasonic"];      
+let guessedLetters = [];
+let guessingArtist = [];
+let usedArtists = [];
+let artistName;
+let numberofGuesses;
+let wins = 0;
+let pause = false; // This var and setTimout function to not listen for keypress while game resets
+      
+  function startYourGame() {
+  
+    artistName = artists[Math.floor(Math.random() * artists.length)].toUpperCase();
+    if (artistName.length <= 6) {
+      numberofGuesses = 8
+    } else if (artistName.length > 6 && artistName.length <= 9) {
+      numberofGuesses = 10
+    } else if (artistName.length > 9) {
+      numberofGuesses = 10
+    } 
+  
+    for (var i=0; i < artistName.length; i++) {
+      if (artistName[i] === " ") {
+        guessingArtist.push(" ")
+      } 
+      else {
+        guessingArtist.push("_");
+      }
+    }
+    updateDisplay();
+  }
+      
+  function resetGame() {
+    if (usedArtists.length === artists.length) {
+      usedArtists = []
+      wins = 0
+      setTimeout(resetGame, 1000); 
+    } else {
+      
+        pause = false;
+      
+        artistName = artists[Math.floor(Math.random() * artists.length)].toUpperCase();
+        console.log(artistName)
+      
+        if (usedArtists.includes(artistName) === true) {
+          resetGame();
+        }
+      
+      
         if (artistName.length <= 6) {
-            numberofGuesses= 8;
-        } else if (artistName.length > 6 && artistName.length <= 9) {
-            numberofGuesses= 10;
-        } else if (artistName.length > 9) {
-            numberofGuesses= 15;
+          numberofGuesses = 8
+        } else if (artistName.length >6 && artistName.length <= 9) {
+          numberofGuesses = 10
+        } else if (artistName.length >9) {
+          numberofGuesses = 15
+        } 
+  
+  
+      for (var i=0; i < artistName.length; i++) {
+
+        if (artistName[i] === " ") {
+
+          guessingArtist.push(" ")
+
+        } 
+        else {
+
+          guessingArtist.push("_");
+
         }
+      }
 
-        for (var i = 0; i < artistName.length; i++) {
+      guessedLetters = [];
+      guessingArtist = [];
+      updateDisplay();
+    }
+  };
+      
+  function updateDisplay () {
 
-            if (artistName[i] === " ") {
-                guessingArtist.push(" ");
-            } else {
-                guessingArtist.push(" ");
-            }
-        }
+    document.getElementById("numberofWins").innerText = wins;
+    document.getElementById("currentArtist").innerText = guessingArtist.join("");
+    document.getElementById("remainingGuesses").innerText = numberofGuesses;
+    document.getElementById("guessedLetters").innerText =  guessedLetters.join(" ");
 
-        updateDisplay();
+  };
+      
+  document.onkeydown = function(event) {
+
+    if (isLetter(event.key) && pause === false) {
+
+      checkingForLetters(event.key.toUpperCase());
 
     }
+  }
+      
+  var isLetter = function(ch){
 
+    return typeof ch === "string" && ch.length === 1
+    && (ch >= "a" && ch <= "z" || ch >= "A" && ch <= "Z");
 
+  };
+      
+  function checkingForLetters(letterPressed) {
 
+    var foundLetter = false;
+      
+    for (var i=0; i < artistName.length; i++) {
 
+      if (letterPressed === artistName[i]) {
 
+        guessingArtist[i] = letterPressed
+        foundLetter = true
 
+        if (guessingArtist.join("") === artistName) {
 
+              wins++
+              usedArtists.push(artistName)
+              console.log(usedArtists)
+              pause = true;
+              updateDisplay();
+              setTimeout(resetGame, 1000);
 
+        }
+      }
+    }
 
+    if (foundLetter === false) {
 
+      if (guessedLetters.includes(letterPressed) === false) {
+         
+            guessedLetters.push(letterPressed)
+            numberofGuesses--
 
+      }
 
+      if (numberofGuesses === 0) {
+         
+        usedArtists.push(artistName);
+        console.log(usedArtists)
+        guessingArtist = artistName.split();
+        pause = true;
+        setTimeout(resetGame, 1000);
 
-
-}
+      }
+    }
+        updateDisplay();
+  };
+      
+      startYourGame();
